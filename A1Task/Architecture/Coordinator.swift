8ae2d677e64b58413a37b1b7b4c2f13a038e1ed2
Jan Podmolík <jan.podmolik@notino.com>
2021-08-16 13:05:55 +0200

    feat: setup architecture

---
+++ A1Task/Architecture/Coordinator.swift
@@ -0,0 +1,77 @@
+//
+//  Coordinator.swift
+//  A1Task
+//
+//  Created by Jan Podmolík on 16.08.2021.
+//
+
+import UIKit
+
+typealias Coordinator<InputDataType> = CoordinatorProtocol & CoordinatorSuperclass<InputDataType>
+
+enum CoordinatorType {
+    case replaceWindow
+    case modal(UIViewController)
+    case push(UINavigationController)
+}
+
+protocol CoordinatorProtocol {
+    associatedtype InputDataType
+
+    var inputData: InputDataType { get }
+
+    func start()
+    func stop()
+}
+
+extension CoordinatorProtocol {
+    func stop() {}
+}
+
+class CoordinatorSuperclass<InputDataType> {
+
+    // MARK: - Properties
+
+    var type: CoordinatorType
+    var inputData: InputDataType
+
+    // MARK: - Init
+
+    init(
+        type: CoordinatorType,
+        inputData: InputDataType
+    ) {
+        self.type = type
+        self.inputData = inputData
+    }
+
+    // MARK: - Lifecycle
+
+    func presentByModal(viewController: UIViewController, animated: Bool = true) {
+        if case .modal(let sourceViewController) = type {
+            sourceViewController.present(viewController, animated: animated)
+        } else {
+            assertionFailure("You are trying to present scene by modal, but you didnt initialized its Coordinator as modal.")
+        }
+    }
+
+    func presentByPush(viewController: UIViewController, animated: Bool = true) {
+        if case .push(let navigationController) = type {
+            navigationController.pushViewController(viewController, animated: animated)
+        } else {
+            assertionFailure("You are trying to present scene by push, but you didnt initialized its Coordinator as push.")
+        }
+    }
+
+    func presentByReplaceWindow(viewController: UIViewController) {
+        if case .replaceWindow = type {
+            if let window = UIApplication.shared.keyWindow {
+                    window.rootViewController = UINavigationController(rootViewController: viewController)
+                } else {
+                    assertionFailure("You are trying to present scene by replaceWindow, but the window was not initialized.")
+                }
+            } else {
+                assertionFailure("You are trying to present scene by replaceWindow, but you didnt initialized its Coordinator as window replacement.")
+            }
+        }
+}
---
+++ A1Task/Launch/AppCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  AppCoordinator.swift
+//  A1Task
+//
+//  Created by Jan Podmolík on 16.08.2021.
+//
+
+import UIKit
+
+final class AppCoordinator {
+
+    var window: UIWindow?
+
+    init() {}
+
+    func start() {}
+}
+
